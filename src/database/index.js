f929cbf031edab4af4f8d5313d7d6fe2fd904465
Mônica Santos <monicasantosdev@gmail.com>
2020-02-02 18:41:29 -0300

    saving files

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize';
-import databaseConfig from '../config/database';
 
+import databaseConfig from '../config/database';
+import File from '../app/models/File';
 import User from '../app/models/User';
 
-const models = [User];
+const models = [File, User];
 
 class Database {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,23 @@
 /* eslint-disable no-console */
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/user', UserController.store);
 routes.post('/session', SessionController.store);
+
 routes.use(authMiddleware);
+
 routes.put('/user', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
